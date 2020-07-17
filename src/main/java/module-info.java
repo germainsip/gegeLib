b914c7b772c99d0747f3590fd9841c0eb2d33eaa
Germain SIPIERE <43010217+germainsip@users.noreply.github.com>
2020-07-17 22:43:29 +0200

    sur win num version snapshot à enlever ok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ open module gegeLib.main {
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.databind;
     requires transitive com.fasterxml.jackson.annotation;
-
+    requires org.apache.logging.log4j;
     requires transitive javafx.fxml;
     requires javafx.graphics;
     requires transitive javafx.controls;

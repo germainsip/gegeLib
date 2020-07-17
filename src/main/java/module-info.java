87fec647d7c857726643a0d36b4c0074b2246047
Germain SIPIERE <gsipiere@gmail.com>
2020-07-17 16:47:49 +0200

    ajout du plugin badass et gestion des modules problème de lecture du json après packaging

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+open module gegeLib.main {
+    requires transitive com.fasterxml.jackson.core;
+    requires transitive com.fasterxml.jackson.databind;
+    requires transitive com.fasterxml.jackson.annotation;
+
+    requires transitive javafx.fxml;
+    requires javafx.graphics;
+    requires transitive javafx.controls;
+    requires javafx.web;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires com.jfoenix;
+
+    exports org.gerblog ;
+}
